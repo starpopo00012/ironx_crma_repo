d3ddd81947ffc48d4c4c4cc2e27829135065dd81
M.JITTAWUT <starpopo00012@gmail.com>
2023-09-19 11:10:40 +0700

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/por/ironx_crma_repo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/por/ws_moveit/devel;/home/por/catkin_ws/devel;/home/por/ros_workshop_ws/devel;/home/por/tutorial_ws/devel;/opt/ros/noetic'.split(';') if '/home/por/ws_moveit/devel;/home/por/catkin_ws/devel;/home/por/ros_workshop_ws/devel;/home/por/tutorial_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/por/ironx_crma_repo/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/por/ironx_crma_repo/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/por/ironx_crma_repo/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/por/ironx_crma_repo/src:$ROS_PACKAGE_PATH"
